b3b6c030aea6e1ac3d6c7a6419e8f882c7cf4b33
FathimahN <fath20233ti@student.nurulfikri.ac.id>
2022-09-24 10:02:41 +0700

    add tugas7_js-jquery to folder tugas

---
+++ tugas_javascript/tugas7_js-jquery_fathimahNuurussa'aadah_STT-NF_henrySaptono/js/script.js
@@ -0,0 +1,260 @@
+$(document).ready(function() {
+  // Hewan Unggas
+  // Ayam
+  $(".chicken").click(function() {
+    $(this).css({
+      background: "url(../images/chicken.png)"
+    });
+    $("<audio></audio>").attr({
+      'src': "../sounds/chicken.mp3",
+      'volume': 0.4,
+      'autoplay': "autoplay"
+    }).appendTo(".content");
+  });
+  // Merpati
+  $(".dove").click(function() {
+    $(this).css({
+      background: "url(../images/dove.png)"
+    });
+    $("<audio></audio>").attr({
+      'src': "../sounds/dove.mp3",
+      'volume': 0.4,
+      'autoplay': "autoplay"
+    }).appendTo(".content");
+  });
+  // Bebek
+  $(".duck").click(function() {
+    $(this).css({
+      background: "url(../images/duck.png)"
+    });
+    $("<audio></audio>").attr({
+      'src': "../sounds/duck.mp3",
+      'volume': 0.4,
+      'autoplay': "autoplay"
+    }).appendTo(".content");
+  });
+  // Ayam Jantan
+  $(".rooster").click(function() {
+    $(this).css({
+      background: "url(../images/rooster.png)"
+    });
+    $("<audio></audio>").attr({
+      'src': "../sounds/rooster.mp3",
+      'volume': 0.4,
+      'autoplay': "autoplay"
+    }).appendTo(".content");
+  });
+  // Angsa
+  $(".swan").click(function() {
+    $(this).css({
+      background: "url(../images/swan.png)"
+    });
+    $("<audio></audio>").attr({
+      'src': "../sounds/swan.mp3",
+      'volume': 0.4,
+      'autoplay': "autoplay"
+    }).appendTo(".content");
+  });
+  // Ayam Kalkun
+  $(".turkey").click(function() {
+    $(this).css({
+      background: "url(../images/turkey.png)"
+    });
+    $("<audio></audio>").attr({
+      'src': "../sounds/turkey.mp3",
+      'volume': 0.4,
+      'autoplay': "autoplay"
+    }).appendTo(".content");
+  });
+  
+  // Hewan Ternak
+  // Domba
+  $(".sheep").click(function() {
+    $(this).css({
+      background: "url(../images/sheep.png)"
+    });
+    $("<audio></audio>").attr({
+      'src': "../sounds/sheep.mp3",
+      'volume': 0.4,
+      'autoplay': "autoplay"
+    }).appendTo(".content");
+  });
+  // Sapi
+  $(".cow").click(function() {
+    $(this).css({
+      background: "url(../images/cow.png)"
+    });
+    $("<audio></audio>").attr({
+      'src': "../sounds/cow.mp3",
+      'volume': 0.4,
+      'autoplay': "autoplay"
+    }).appendTo(".content");
+  });
+  // Kerbau
+  $(".buffalo").click(function() {
+    $(this).css({
+      background: "url(../images/buffalo.png)"
+    });
+    $("<audio></audio>").attr({
+      'src': "../sounds/buffalo.mp3",
+      'volume': 0.4,
+      'autoplay': "autoplay"
+    }).appendTo(".content");
+  });
+  // Alpaka
+  $(".alpaca").click(function() {
+    $(this).css({
+      background: "url(../images/alpaca.png)"
+    });
+    $("<audio></audio>").attr({
+      'src': "../sounds/alpaca.mp3",
+      'volume': 0.4,
+      'autoplay': "autoplay"
+    }).appendTo(".content");
+  });
+  // Lebah
+  $(".bee").click(function() {
+    $(this).css({
+      background: "url(../images/bee.png)"
+    });
+    $("<audio></audio>").attr({
+      'src': "../sounds/bee.mp3",
+      'volume': 0.4,
+      'autoplay': "autoplay"
+    }).appendTo(".content");
+  });
+  
+  // Hewan Buas
+  // Buaya
+  $(".alligator").click(function() {
+    $(this).css({
+      background: "url(../images/alligator.png)"
+    });
+    $("<audio></audio>").attr({
+      'src': "../sounds/alligator.mp3",
+      'volume': 0.4,
+      'autoplay': "autoplay"
+    }).appendTo(".content");
+  });
+  // Hiu
+  $(".shark").click(function() {
+    $(this).css({
+      background: "url(../images/shark.png)"
+    });
+    $("<audio></audio>").attr({
+      'src': "../sounds/shark.mp3",
+      'volume': 0.4,
+      'autoplay': "autoplay"
+    }).appendTo(".content");
+  });
+  // Harimau 
+  $(".tiger").click(function() {
+    $(this).css({
+      background: "url(../images/tiger.png)"
+    });
+    $("<audio></audio>").attr({
+      'src': "../sounds/tiger.mp3",
+      'volume': 0.4,
+      'autoplay': "autoplay"
+    }).appendTo(".content");
+  });
+  // Singa
+  $(".lion").click(function() {
+    $(this).css({
+      background: "url(../images/lion.png)"
+    });
+    $("<audio></audio>").attr({
+      'src': "../sounds/lion.mp3",
+      'volume': 0.4,
+      'autoplay': "autoplay"
+    }).appendTo(".content");
+  });
+  // Komodo
+  $(".komodo-dragon").click(function() {
+    $(this).css({
+      background: "url(../images/komodo-dragon.png)"
+    });
+    $("<audio></audio>").attr({
+      'src': "../sounds/komodo-dragon.mp3",
+      'volume': 0.4,
+      'autoplay': "autoplay"
+    }).appendTo(".content");
+  });
+  // Ular Derik
+  $(".rattlesnake").click(function() {
+    $(this).css({
+      background: "url(../images/rattlesnake.png)"
+    });
+    $("<audio></audio>").attr({
+      'src': "../sounds/rattlesnake.mp3",
+      'volume': 0.4,
+      'autoplay': "autoplay"
+    }).appendTo(".content");
+  });
+  // Serigala 
+  $(".wolf").click(function() {
+    $(this).css({
+      background: "url(../images/wolf.png)"
+    });
+    $("<audio></audio>").attr({
+      'src': "../sounds/wolf.mp3",
+      'volume': 0.4,
+      'autoplay': "autoplay"
+    }).appendTo(".content");
+  });
+  // Macan Tutul
+  $(".leopard").click(function() {
+    $(this).css({
+      background: "url(../images/leopard.png)"
+    });
+    $("<audio></audio>").attr({
+      'src': "../sounds/leopard.mp3",
+      'volume': 0.4,
+      'autoplay': "autoplay"
+    }).appendTo(".content");
+  });
+  // Hiena 
+  $(".hyena").click(function() {
+    $(this).css({
+      background: "url(../images/hyena.png)"
+    });
+    $("<audio></audio>").attr({
+      'src': "../sounds/hyena.mp3",
+      'volume': 0.4,
+      'autoplay': "autoplay"
+    }).appendTo(".content");
+  });
+  // Kuda Nil
+  $(".hippopotamus").click(function() {
+    $(this).css({
+      background: "url(../images/hippopotamus.png)"
+    });
+    $("<audio></audio>").attr({
+      'src': "../sounds/hippopotamus.mp3",
+      'volume': 0.4,
+      'autoplay': "autoplay"
+    }).appendTo(".content");
+  });
+  // Kera
+  $(".ape").click(function() {
+    $(this).css({
+      background: "url(../images/ape.png)"
+    });
+    $("<audio></audio>").attr({
+      'src': "../sounds/ape.mp3",
+      'volume': 0.4,
+      'autoplay': "autoplay"
+    }).appendTo(".content");
+  });
+  // Kalajengking
+  $(".scorpion").click(function() {
+    $(this).css({
+      background: "url(../images/scorpion.png)"
+    });
+    $("<audio></audio>").attr({
+      'src': "../sounds/scorpion.mp3",
+      'volume': 0.4,
+      'autoplay': "autoplay"
+    }).appendTo(".content");
+  });
+});
